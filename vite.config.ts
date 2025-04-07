39acbf45ccd312903f551b1e4ae10ad5c5304a2b
Soufiane Asrih <soufasri@emeal.nttdata.com>
2025-04-07 22:59:30 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // Set base URL to relative path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
